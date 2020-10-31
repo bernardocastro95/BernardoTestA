f72c22c36e0d5f0109261329d287eda999b90760
Bernardo Castro <bernardoc544@gmail.com>
2020-10-31 09:21:02 -0400

    Only Commity

---
+++ bernardoTestA/src/main/java/bernardo/castro/s301073235/BernardoActivity.java
@@ -0,0 +1,14 @@
+package bernardo.castro.s301073235;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BernardoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bernardo);
+    }
+}
